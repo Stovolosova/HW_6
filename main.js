624e3ecbf69aa7ecfb21c9c490b83382b2886767
stovolosova <stovolosova.helen@gmail.com>
2023-06-07 16:40:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch (true) {
+    case (numOrStr === null):
+        alert ('ви відмінили');
+        break;
+    case (numOrStr.trim () === ''):
+        alert('Empty String');
+        break;
+    case isNaN ( +numOrStr ):
+        alert('number is Ba_NaN');
+        break;
+    default:
+        alert ('OK!');
+}
